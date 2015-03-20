004edbb696937f36620cb8695d259045d84ee4aa
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-03-20 12:28:27 +0100

    discommented source and target frames needed for the transformer

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -42,19 +42,19 @@ bool Task::configureHook()
     // No configuration needed apparently
     if (! TaskBase::configureHook())
         return false;
-    
+
     if (!_origin.value().hasValidPosition() || !_origin.value().hasValidOrientation() ) 
         _origin.set(getZeroOrigin());
 
     if (!_body_reference.value().hasValidPosition() || !_body_reference.value().hasValidOrientation() ) 
         _body_reference.set(getZeroOrigin());
-    
+
     return true;
 }
 bool Task::startHook()
 {
     mpARTDriver->connect(_host.value(),_server_port.value(),_data_port.value());
-    
+
     return true;
 }
 void Task::updateHook()
@@ -67,11 +67,11 @@ void Task::updateHook()
     base::samples::RigidBodyState rbs;
     rbs.time = mpARTDriver->getTimestamp();
 
-    //rbs.sourceFrame = _source_frame.get();
-    //rbs.targetFrame = _target_frame.get();
+    rbs.sourceFrame = _source_frame.get();
+    rbs.targetFrame = _target_frame.get();
 
     Eigen::Affine3d segment_transform = mpARTDriver->getSegmentTransform(_numBody.value());
-    
+
 
     rbs.setTransform( C_world2origin * segment_transform * C_segment2body );
 
